a2fafef413d23e23469980047c51bc42c69a1de2
Andy Schmitz <andy.schmitz@gmail.com>
2015-09-18 08:41:56 -0500

    Update Ruby read script to dump data to RethinkDB

--- read_ruby/read.rb
+++ read_ruby/read.rb
@@ -2,33 +2,80 @@
 require 'rubygems'
 require 'hexdump'
 require 'zlib'
+require 'digest'
+require 'rethinkdb'
+include RethinkDB::Shortcuts
+
+conn = r.connect(host: "localhost", port: 28015)
 
 Encoding.default_external = 'BINARY'
+STATIC_HEADER = "gpak\x00\x0d\x0a\xa5"
+VERSION_NUMBER = "\x00\x00\x00\x01"
+EMPTY_TIMESTAMP = "\xff" * 8
+EMPTY_CHECKSUM = "\x00" * 32
+TYPE_DELTA = 0x08
+TYPE_COMPRESSED = 0x10
+TYPE_INVALID_MASK = 0xff ^ TYPE_COMPRESSED ^ TYPE_DELTA ^ 0x07
 
-marked_object = '78afc6473f30ba4ee28b4f04655cbb6b67456607'
-pack = File.open('file.globpack')
+if ARGV.length < 1 || ARGV.length > 2
+  puts "Usage: read.rb globpack [hash to output]"
+  exit
+end
+pack = File.open(ARGV[0])
+globpack_name = File.basename(ARGV[0])
+marked_object = [ARGV[1]].pack('H40')
 
+# Read and verify header (sans checksum, we'll do that later)
 header = pack.read(52)
+if header.length != 52
+  raise 'Unexpectedly short globpack/header'
+end
+if header[0..7] != STATIC_HEADER
+  raise 'Bad globpack static header.'
+end
+if header[8..11] != VERSION_NUMBER
+  raise 'Unknown globpack version.'
+end
+expected_length = header[12..19].unpack('Q>').first
+if File.size(pack) != expected_length
+  raise "Incorrect globpack length. (File said #{expected_length}, really was "+
+    File.size(pack)+')'
+end
+expected_checksum = header[20..51]
 pos = 52
+
+# Initialize checksum. Note that the checksum is computed with itself and the
+# size being masked (each in a different way)
+checksum_header = header.dup
+checksum_header[12..19] = EMPTY_TIMESTAMP
+checksum_header[20..51] = EMPTY_CHECKSUM
+checksum_digest = Digest::SHA256.new
+checksum_digest.update checksum_header
 
 deltas = 0
 compressed = 0
 objects = {}
 while not pack.eof?
   objpos = pos
-  hash = pack.read(20).unpack('H40')[0]
-  type = pack.readbyte
+  hash_data = pack.read(20)
+  checksum_digest.update hash_data
+  
+  type = pack.read(1)
+  checksum_digest.update type
+  type = type.unpack('C').first
   pos += 21
-  if type & 0xe0 > 0
+  
+  if type & TYPE_INVALID_MASK > 0
     raise "Unexpected type: #{type}"
   end
-  if type & 0x08 > 0
+  if type & TYPE_DELTA > 0
     # This is a delta, read a base object too.
     base = pack.read(20)
+    checksum_digest.update(base)
     pos += 20
     deltas += 1
   end
-  if type & 0x10 > 0
+  if type & TYPE_COMPRESSED > 0
     compressed += 1
   end
   # if type & 0x07 == 0x01
@@ -40,29 +87,52 @@ while not pack.eof?
   offset = 0
   length = 0
   while continue
-    byte = pack.readbyte
+    byte = pack.read(1)
+    checksum_digest.update(byte)
+    byte = byte.unpack('C').first
     continue = byte & 0x80 == 0x80
     length += (byte & 0x7f) << offset
     offset += 7
     pos += 1
   end
   
-  # puts "hash: #{hash}, type: #{type}, length: #{length}, loc: #{objpos}"
-  objects[hash] = objpos
+  objects[hash_data] = objpos
   
   compressed_data = pack.read(length)
+  checksum_digest.update(compressed_data)
   pos += length
   
-  if hash == marked_object
-    puts "Marked object (#{hash}):"
+  if hash_data == marked_object
+    puts "Marked object (#{hash_data.unpack('H40')[0]}):"
     puts "  Type: 0x#{type.chr.unpack('H2')[0]}"
-    puts "Compressed data:"
-    compressed_data.hexdump
-    decompressed_data = Zlib::Inflate.new(-15).inflate(compressed_data)
+    if type & TYPE_COMPRESSED
+      puts "Compressed data:"
+      compressed_data.hexdump
+      decompressed_data = Zlib::Inflate.new(-15).inflate(compressed_data)
+    else
+      decompressed_data = compressed_data
+    end
     puts "Decompressed data:"
     decompressed_data.hexdump
   end
 end
 pack.close
 
+computed_checksum = checksum_digest.digest
+if expected_checksum != computed_checksum
+  raise 'Checksum mismatch'
+end
+
 puts "#{objects.length} objects, #{deltas} deltas, #{compressed} compressed"
+
+to_insert = objects.map{|hash, loc| {
+  id: r.binary(hash),
+  loc: loc,
+  file: globpack_name
+}}
+res = r.db('gitglob_2').table('objects').insert(to_insert,
+  conflict: 'update').run(conn)
+if res['errors'] > 0
+  raise "RethinkDB Error(s): #{res['errors']} errors. First error:\n"+
+    res['first_error']
+end
